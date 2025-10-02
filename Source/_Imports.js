3c137bee7b1583374f311c330804ba230714d29a
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-10-01 23:03:03 -0400

    Removed _Imports.ts.

--- Source/_Imports.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var ses = ThisCouldBeBetter.SoundEffectSynthesizer;
-var SoundSequence = ses.SoundSequence;
-var SoundSequenceNote = ses.SoundSequenceNote;
-var SoundSequenceVoice = ses.SoundSequenceVoice;
-var UiEventHandler = ses.UiEventHandler;
--- Source/_Imports.ts
+++
@@ -1,6 +0,0 @@
-import ses = ThisCouldBeBetter.SoundEffectSynthesizer;
-
-import SoundSequence = ses.SoundSequence;
-import SoundSequenceNote = ses.SoundSequenceNote;
-import SoundSequenceVoice = ses.SoundSequenceVoice;
-import UiEventHandler = ses.UiEventHandler;
