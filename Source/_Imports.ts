c865fafa41223f48136bf49d80351dbddc147901
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-10-05 00:28:34 -0400

    Added _Imports.ts, for downstream projects to use.

---
+++ Source/_Imports.ts
@@ -0,0 +1,6 @@
+
+import ses = ThisCouldBeBetter.SoundEffectSynthesizer;
+
+import SoundSequence = ses.SoundSequence;
+import SoundSequenceNote = ses.SoundSequenceNote;
+import SoundSequenceVoice = ses.SoundSequenceVoice;
